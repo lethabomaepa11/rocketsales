60920999132b8f43c621d83c87dc8fbc147605fd
Lethabo Maepa <lethabo.maepa@boxfusion.io>
2026-02-25 10:51:59 +0200

    Installed and configured antd, redux actions and other necessary packages

---
+++ src/app/utils/themeConfig.ts
@@ -0,0 +1,11 @@
+// theme/themeConfig.ts
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: '#DF6D27',
+  },
+};
+
+export default theme;
